0879d6bdf9d76389c42d4441a3c078e06da2547b
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2024-09-14 07:37:55 -0400

    test(vitest): impl 'commonVitestNodeConfigs' to dedupe configs

--- apps/fixit-api/vitest.config.ts
+++ apps/fixit-api/vitest.config.ts
@@ -1,39 +1,19 @@
-import viteTsconfigPaths from "vite-tsconfig-paths";
-import GithubActionsReporter from "vitest-github-actions-reporter";
-import { defineConfig, coverageConfigDefaults } from "vitest/config";
+import { defineConfig, mergeConfig } from "vitest/config";
+import { commonVitestNodeConfigs } from "../../libs/vitest-utils/common-configs/vitest.node-configs.js";
 
-export default defineConfig({
-  plugins: [viteTsconfigPaths()],
-  test: {
-    /* `restoreMocks` accomplishes the following:
-      - clears all spies of `spy.mock.calls` and `spy.mock.results` (same as clearMocks:true)
-      - removes any mocked implementations (same as mockReset:true)
-      - restores the original implementation so fns don't return undefined like with mockReset */
-    restoreMocks: true,
-    globals: true,
-    silent: true,
-    environment: "node",
-    include: ["**/?(*.)test.?(c|m)[tj]s"],
-    setupFiles: ["src/tests/setupTests.ts"],
-    server: {
-      deps: {
-        // This config allows mocking the package's underlying @aws-sdk imports
-        inline: ["@nerdware/ddb-single-table"],
+// Note: currently the nx vite plugin breaks if we use path aliases in vite/vitest config files.
+
+export default mergeConfig(
+  commonVitestNodeConfigs,
+  defineConfig({
+    test: {
+      setupFiles: ["vitest.setup.ts"],
+      server: {
+        deps: {
+          // This config allows mocking the package's underlying @aws-sdk imports
+          inline: ["@nerdware/ddb-single-table"],
+        },
       },
     },
-    reporters: [
-      "default",
-      // GithubActionsReporter is used to format test results for GitHub Actions
-      ...(process.env.GITHUB_ACTIONS ? [new GithubActionsReporter()] : []),
-    ],
-    coverage: {
-      include: ["src/**/*.ts"],
-      exclude: [...coverageConfigDefaults.exclude, "**/tests/**/*", "**/__mocks__/**/*"],
-      reporter: [
-        ...coverageConfigDefaults.reporter,
-        // 'json-summary' is used by the vitest-coverage-report GitHub Action
-        ...(process.env.GITHUB_ACTIONS ? ["json-summary"] : []),
-      ],
-    },
-  },
-});
+  })
+);
