4cd12b808f9cc4e7ddb2395c5f13451134cb50fd
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2024-09-15 12:23:40 -0400

    fix: update ENV property access

--- apps/fixit-api/src/server/logStartupInfo.ts
+++ apps/fixit-api/src/server/logStartupInfo.ts
@@ -1,22 +1,16 @@
-import { ENV } from "@/server/env";
+import { ENV } from "@/server/env.js";
 import { logger } from "@/utils/logger.js";
-
-const {
-  NODE_ENV,
-  AWS,
-  CONFIG: { PROJECT_VERSION, TIMEZONE, OS_PLATFORM, PID, NODE_VERSION, API_BASE_URL, PORT, CWD },
-} = ENV;
 
 if (ENV.IS_DEV) {
   logger.server(
-    `(SERVER STARTUP) 🚀 fixit-api ${PROJECT_VERSION || ""}
-    NODE_ENV...... ${NODE_ENV}
-    AWS Region ... ${AWS.REGION}
-    Timezone ..... ${TIMEZONE}
-    Platform ..... ${OS_PLATFORM}
-    PID .......... ${PID}
-    NodeJS ....... ${NODE_VERSION}
-    Host ......... ${API_BASE_URL}:${PORT}
-    CWD .......... ${CWD}`
+    `(SERVER STARTUP) 🚀 fixit-api ${ENV.PROJECT_VERSION || ""}
+    NODE_ENV...... ${ENV.NODE_ENV}
+    AWS Region ... ${ENV.AWS_REGION}
+    Timezone ..... ${/([A-Z]+[+-][0-9]+.*)/.exec(new Date().toString())?.[0] ?? "-"}
+    Platform ..... ${process.platform}
+    PID .......... ${process.pid}
+    NodeJS ....... ${process.version}
+    Host ......... ${ENV.API_BASE_URL}:${ENV.PORT}
+    CWD .......... ${process.cwd()}`
   );
 }
