c7a0d67999e46af7b36598692e5cf51e73b12b03
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2024-09-17 13:07:50 -0400

    docs: rm extraneous comment

--- apps/fixit-api/vitest.config.ts
+++ apps/fixit-api/vitest.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig, mergeConfig } from "vitest/config";
 import { commonVitestNodeConfigs } from "../../libs/vitest-utils/common-configs/vitest.node-configs.js";
 
-// Note: currently the nx vite plugin breaks if we use path aliases in vite/vitest config files.
-
 export default mergeConfig(
   commonVitestNodeConfigs,
   defineConfig({
