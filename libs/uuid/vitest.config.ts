8c01db1ecb1b02515ad29d42afa2fe5788332b20
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2024-09-17 12:54:44 -0400

    docs: rm extraneous comment

--- libs/uuid/vitest.config.ts
+++ libs/uuid/vitest.config.ts
@@ -1,3 +1,1 @@
-export { commonVitestNodeConfigs as default } from "../../libs/vitest-utils/common-configs/vitest.node-configs.js";
-
-// Note: currently the nx vite plugin breaks if we use path aliases in vite/vitest config files.
+export { commonVitestNodeConfigs as default } from "../vitest-utils/common-configs/vitest.node-configs.js";
