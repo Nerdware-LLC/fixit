c3b9ea7b6029456c3b84af31eb15e4f0aac4732e
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2024-09-17 13:09:24 -0400

    fix: update ENV import path + PORT property access

--- apps/fixit-api/src/index.ts
+++ apps/fixit-api/src/index.ts
@@ -1,8 +1,8 @@
 import "@/server/init.js";
 import { httpServer } from "@/httpServer.js";
-import { ENV } from "@/server/env";
+import { ENV } from "@/server/env.js";
 import { logger } from "@/utils/logger.js";
 
-await httpServer.start({ port: ENV.CONFIG.PORT }, () => {
+await httpServer.start({ port: ENV.PORT }, () => {
   logger.server("👂 Server is listening.");
 });
