3440a2ad2c701615d93839c35387b4221e4330bf
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2024-09-17 12:55:00 -0400

    docs(jsdoc): rm "invalid" jsdoc tag

--- libs/uuid/src/common.ts
+++ libs/uuid/src/common.ts
@@ -9,7 +9,8 @@ export const isValidUUID = (value: unknown): value is string => {
 
 /**
  * Regex for validating UUID strings.
- * @source https://github.com/uuidjs/uuid/blob/6dcb15b86357afdf29ae2dabf5b2a8afab83c2c0/src/regex.js
+ *
+ * Regex source: https://github.com/uuidjs/uuid/blob/6dcb15b86357afdf29ae2dabf5b2a8afab83c2c0/src/regex.js
  */
 export const UUID_REGEX =
   /^(?:[0-9a-f]{8}-[0-9a-f]{4}-[1-8][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}|00000000-0000-0000-0000-000000000000|ffffffff-ffff-ffff-ffff-ffffffffffff)$/i;
