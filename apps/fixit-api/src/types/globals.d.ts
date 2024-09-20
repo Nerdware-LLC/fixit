6ddf7ec79c3b0c5ed00069081401a793aa872b35
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2024-09-20 08:34:24 -0400

    feat: add process.env related types to simplify use of env vars

--- apps/fixit-api/src/types/globals.d.ts
+++ apps/fixit-api/src/types/globals.d.ts
@@ -1,49 +1,50 @@
-import type { JsonValue } from "type-fest";
+import type { Algorithm } from "jsonwebtoken";
+import type { Stripe } from "stripe";
+import type { JsonValue, OmitIndexSignature, OverrideProperties } from "type-fest";
 
 declare global {
   namespace NodeJS {
     /**
-     * process.env fields read by the src/server/env module. These env vars are
-     * often not provided in test and CI environments, and are therefore optional
-     * (with the exception of NODE_ENV, which should always be defined).
+     * This declaration adds custom fields to the `process.env` object type.
      */
     interface ProcessEnv {
-      NODE_ENV?: "development" | "test" | "staging" | "production" | undefined;
-      npm_package_version?: string | undefined;
+      readonly NODE_ENV?: "development" | "test" | "staging" | "production" | undefined;
+      readonly npm_package_version?: string | undefined;
       // SERVER
-      PROTOCOL?: string | undefined;
-      DOMAIN?: string | undefined;
-      PORT?: string | undefined;
+      readonly PROTOCOL?: string | undefined;
+      readonly DOMAIN?: string | undefined;
+      readonly PORT?: string | undefined;
       // WEB CLIENT
-      WEB_CLIENT_URL?: string | undefined;
+      readonly WEB_CLIENT_URL?: string | undefined;
       // AWS
-      AWS_REGION?: string | undefined;
-      DYNAMODB_REGION?: string | undefined;
-      DYNAMODB_TABLE_NAME?: string | undefined;
-      DYNAMODB_ENDPOINT?: string | undefined;
-      PINPOINT_PROJECT_ID?: string | undefined;
-      SES_EMAIL_ADDRESS?: string | undefined;
+      readonly AWS_REGION?: string | undefined;
+      readonly DYNAMODB_REGION?: string | undefined;
+      readonly DYNAMODB_TABLE_NAME?: string | undefined;
+      readonly DYNAMODB_ENDPOINT?: string | undefined;
+      readonly PINPOINT_PROJECT_ID?: string | undefined;
+      readonly SES_EMAIL_ADDRESS?: string | undefined;
       // AUTH
-      JWT_PRIVATE_KEY?: string | undefined;
-      JWT_ALGORITHM?: string | undefined;
-      JWT_ISSUER?: string | undefined;
-      JWT_EXPIRES_IN?: string | undefined;
-      BCRYPT_SALT_ROUNDS?: string | undefined;
-      UUID_NAMESPACE?: string | undefined;
+      readonly JWT_PRIVATE_KEY?: string | undefined;
+      readonly JWT_ALGORITHM?: Algorithm | undefined;
+      readonly JWT_ISSUER?: string | undefined;
+      readonly JWT_EXPIRES_IN?: string | undefined;
+      readonly BCRYPT_SALT_ROUNDS?: string | undefined;
+      readonly UUID_NAMESPACE?: string | undefined;
       // SENTRY
-      SENTRY_DSN?: string | undefined;
+      readonly SENTRY_DSN?: string | undefined;
       // STRIPE
-      STRIPE_WEBHOOKS_SECRET?: string | undefined;
-      STRIPE_PUBLISHABLE_KEY?: string | undefined;
-      STRIPE_SECRET_KEY?: string | undefined;
+      readonly STRIPE_API_VERSION?: Stripe.StripeConfig["apiVersion"] | undefined;
+      readonly STRIPE_PUBLISHABLE_KEY?: string | undefined;
+      readonly STRIPE_SECRET_KEY?: string | undefined;
+      readonly STRIPE_WEBHOOKS_SECRET?: string | undefined;
       // GOOGLE
-      GOOGLE_OAUTH_CLIENT_ID?: string | undefined;
-      GOOGLE_OAUTH_CLIENT_SECRET?: string | undefined;
+      readonly GOOGLE_OAUTH_CLIENT_ID?: string | undefined;
+      readonly GOOGLE_OAUTH_CLIENT_SECRET?: string | undefined;
     }
   }
 
   /**
-   * This declaration adds `JSON.parse` overload that replaces `any` with {@link JsonValue}.
+   * This declaration adds a `JSON.parse` overload that replaces `any` with {@link JsonValue}.
    */
   interface JSON {
     parse(
@@ -52,3 +53,23 @@ declare global {
     ): JsonValue;
   }
 }
+
+/**
+ * Union of field-names added to the {@link NodeJS.ProcessEnv|process.env} typing.
+ */
+export type ProcessEnvCustomFieldNames = keyof Omit<OmitIndexSignature<NodeJS.ProcessEnv>, "TZ">;
+
+/**
+ * Dict of custom fields added to the {@link NodeJS.ProcessEnv|process.env} typing.
+ */
+export type ProcessEnvCustomFields = OverrideProperties<
+  {
+    readonly [Key in ProcessEnvCustomFieldNames]-?: NonNullable<NodeJS.ProcessEnv[Key]>;
+  },
+  {
+    readonly PORT: number;
+    readonly BCRYPT_SALT_ROUNDS: number;
+    readonly SENTRY_DSN: string | undefined;
+    readonly DYNAMODB_ENDPOINT: string | undefined;
+  }
+>;
