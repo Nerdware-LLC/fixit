c454aca0442be068fbca1ab2ff5b9a02b59001cc
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2024-10-03 10:40:24 -0400

    feat: impl applyConstraintDirectiveToSchema

--- apps/fixit-api/src/graphql/schema.ts
+++ apps/fixit-api/src/graphql/schema.ts
@@ -1,11 +1,16 @@
 import { makeExecutableSchema } from "@graphql-tools/schema";
-import { typeDefs } from "@fixit/api-schemas/GraphQL/typeDefs";
+import { typeDefs } from "@fixit/api-schemas/GraphQL/__generated__.graphqlSchemaTypeDefs.js";
+import { applyConstraintDirectiveToSchema } from "@fixit/api-schemas/GraphQL/directives/constraint";
 import { resolvers } from "./resolvers.js";
+import type { ApolloServerContext } from "@fixit/apollo-graphql/types";
+
+// The base schema returned by `makeExecutableSchema`:
+const baseExecutableSchema = makeExecutableSchema<ApolloServerContext>({
+  typeDefs,
+  resolvers,
+});
 
 /**
  * Fixit API GraphQL Schema
  */
-export const schema = makeExecutableSchema({
-  typeDefs,
-  resolvers,
-});
+export const schema = applyConstraintDirectiveToSchema(baseExecutableSchema);
