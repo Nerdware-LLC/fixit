7ecfc613be1f2baf5c9d8fbf818f54b3503711fd
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2024-10-03 10:30:44 -0400

    feat: rm dry-run flag, impl apolloRoverCliWrapper.graph.lint

--- libs/api-schemas/GraphQL/scripts/publishGqlSchema.js
+++ libs/api-schemas/GraphQL/scripts/publishGqlSchema.js
@@ -1,7 +1,7 @@
+import { resolve as pathResolve } from "node:path";
 import { logger, validateExecContext } from "@fixit-tools/script-utils";
 import commandLineArgs from "command-line-args";
 import { apolloRoverCliWrapper } from "./apolloRoverCliWrapper.js";
-import { generateGqlSchemaFile } from "./generateGqlSchemaFile.js";
 
 /**
  * This script is used to publish the GraphQL schema to Apollo Studio.
@@ -14,41 +14,34 @@ import { generateGqlSchemaFile } from "./generateGqlSchemaFile.js";
  *
  * OPTIONS
  *     --variant  The GraphQL graph variant to publish (e.g., "--variant=current").
- *     --dryRun   If set, the script will not publish the schema to Apollo Studio.
  *
  * Set the `DEBUG_MODE` environment variable to `true` to log debug messages.
  */
 
 logger.debug("Starting Script ...");
 
-validateExecContext();
+const { cwd } = validateExecContext();
 
-const { variant: variantScriptArg, dryRun: isDryRun } = commandLineArgs([
-  { name: "variant", type: String },
-  { name: "dryRun", type: Boolean, defaultValue: false },
-]);
+const { variant } = commandLineArgs([{ name: "variant", type: String }]);
 
-if (!/^(prod|staging|current)$/.test(variantScriptArg))
+if (!/^(prod|staging|current)$/.test(variant))
   throw new Error(
-    `Invalid --variant script argument: "${variantScriptArg}".\n` +
+    `Invalid --variant script argument: "${variant}".\n` +
       `The GraphQL graph variant must be one of "current", "staging", or "prod".`
   );
 
-const graphRef = `fixit@${variantScriptArg}`;
+const graphRef = `fixit@${variant}`;
 
-const schemaFilePath = await generateGqlSchemaFile(graphRef, {
-  shouldValidate: true,
-  shouldCheckForBreakingChanges: false, // TODO Set to true once the schema is stable
-});
-
-if (isDryRun) {
-  logger.info("Dry-run mode enabled. Skipping schema publication.");
-  process.exit(0);
-}
-
-logger.debug(
-  `Publishing the GraphQL schema for graph "${graphRef}" to Apollo Studio ...`
+const schemaFilePath = pathResolve(
+  cwd,
+  "libs/api-schemas/GraphQL/schema/__generated__.schema.graphqls"
 );
+
+logger.debug(`Linting GraphQL schema file "${schemaFilePath}" ...`);
+
+apolloRoverCliWrapper.graph.lint(graphRef, schemaFilePath);
+
+logger.debug(`Publishing GraphQL schema "${graphRef}" to Apollo Studio ...`);
 
 apolloRoverCliWrapper.graph.publish(graphRef, schemaFilePath);
 
