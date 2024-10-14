d85107e748ae65f4d775baad277787aef2d53b63
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2024-10-14 10:50:42 -0400

    feat: rm the old ID-based constraint formats
    The relevant formats have been extracted into explicit GraphQLScalarType classes

--- libs/api-schemas/GraphQL/src/directives/constraint/constraintValidators/string.ts
+++ libs/api-schemas/GraphQL/src/directives/constraint/constraintValidators/string.ts
@@ -1,8 +1,4 @@
 import { isValidEmail, isValidHandle, isValidPhone, isValidURL } from "@nerdware/ts-string-helpers";
-import { contactModelHelpers } from "@fixit/dynamodb-models/Contact/helpers.js";
-import { invoiceModelHelpers } from "@fixit/dynamodb-models/Invoice/helpers.js";
-import { userModelHelpers } from "@fixit/dynamodb-models/User/helpers.js";
-import { workOrderModelHelpers } from "@fixit/dynamodb-models/WorkOrder/helpers.js";
 import { isValidUUID } from "@fixit/uuid";
 import type { SupportedConstraintFormat } from "@fixit/api-schemas/GraphQL/types";
 import type { ConstraintValidatorsMap } from "./types.js";
@@ -10,17 +6,11 @@ import type { ConstraintValidatorsMap } from "./types.js";
 const STRING_FORMAT_VALIDATORS: Readonly<
   Record<SupportedConstraintFormat, { isValid: (value?: unknown) => boolean; description: string }>
 > = {
-  // MISC FORMATS:
   email: { isValid: isValidEmail, description: "email address" },
   handle: { isValid: isValidHandle, description: `social media handle (e.g., "@foo_user")` },
   phone: { isValid: isValidPhone, description: "10-digit US phone number" },
   url: { isValid: isValidURL, description: "URL" },
   uuid: { isValid: isValidUUID, description: "UUID" },
-  // ID-BASED FORMATS:
-  contactID: { isValid: contactModelHelpers.id.isValid, description: "Contact ID" },
-  invoiceID: { isValid: invoiceModelHelpers.id.isValid, description: "Invoice ID" },
-  userID: { isValid: userModelHelpers.id.isValid, description: "User ID" },
-  workOrderID: { isValid: workOrderModelHelpers.id.isValid, description: "Work Order ID" },
 };
 
 export const STRING_CONSTRAINT_VALIDATORS: ConstraintValidatorsMap<string> = {
