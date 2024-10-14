8067282a2859d99f0fe971ab08ae2b387e378a7a
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2024-10-14 10:53:46 -0400

    fix: update `assignedTo` to `assignedToUserID`

--- apps/fixit-api/src/services/InvoiceService/createInvoice.ts
+++ apps/fixit-api/src/services/InvoiceService/createInvoice.ts
@@ -8,7 +8,7 @@ import type { InvoiceInput } from "@fixit/api-schemas/GraphQL/types";
 export const createInvoice = async (invInput: { createdByUserID: string } & InvoiceInput) => {
   const createdInvoice = await Invoice.createItem({
     createdByUserID: invInput.createdByUserID,
-    assignedToUserID: invInput.assignedTo,
+    assignedToUserID: invInput.assignedToUserID,
     amount: invInput.amount,
     status: "OPEN",
     ...(invInput.workOrderID && {
